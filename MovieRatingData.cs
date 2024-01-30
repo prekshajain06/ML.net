985e977e33f7961fe547190a9ac2c2648baf9039
prekshajain06 <85805109+prekshajain06@users.noreply.github.com>
2024-01-30 12:57:33 +0530

    Add files via upload

---
+++ MovieRatingData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace MovieRecommendation
+{
+    public class MovieRating
+    {
+        [LoadColumn(0)]
+        public float userId;
+        [LoadColumn(1)]
+        public float movieId;
+        [LoadColumn(2)]
+        public float Label;
+    }
+    public class MovieRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+}
